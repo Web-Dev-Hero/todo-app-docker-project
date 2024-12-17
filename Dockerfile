f7467e75038621231b3297335b3ee860a1a1bb72
root <root@ip-172-31-30-149.us-west-2.compute.internal>
2024-12-17 18:12:41 +0000

    first commit

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+
+
+
+FROM python:3.10
+
+WORKDIR /app
+
+COPY requirements.txt .
+
+
+RUN pip install   -r requirements.txt
+RUN pip install flask flask-mysqldb
+
+COPY . .
+
+CMD ["python","app.py"]
---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+# MySQL Configuration
+app.config['MYSQL_HOST'] = 'mysql'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'todo_app'
+
+mysql = MySQL(app)
+
+# Route to display tasks
+@app.route('/')
+def index():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM tasks")
+    tasks = cur.fetchall()
+    cur.close()
+    return render_template('index.html', tasks=tasks)
+
+# Route to add a new task
+@app.route('/add', methods=['POST'])
+def add_task():
+    if request.method == 'POST':
+        title = request.form['title']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO tasks (title) VALUES (%s)", [title])
+        mysql.connection.commit()
+        cur.close()
+    return redirect(url_for('index'))
+
+# Route to delete a task
+@app.route('/delete/<int:task_id>')
+def delete_task(task_id):
+    cur = mysql.connection.cursor()
+    cur.execute("DELETE FROM tasks WHERE id = %s", [task_id])
+    mysql.connection.commit()
+    cur.close()
+    return redirect(url_for('index'))
+
+# Route to mark task as complete
+@app.route('/complete/<int:task_id>')
+def complete_task(task_id):
+    cur = mysql.connection.cursor()
+    cur.execute("UPDATE tasks SET status = TRUE WHERE id = %s", [task_id])
+    mysql.connection.commit()
+    cur.close()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
